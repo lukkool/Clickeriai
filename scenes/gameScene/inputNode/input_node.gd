5b7ca38b74f02507dd889affce226758012df7e1
Lukas Lisauskas <90777750+lukkool@users.noreply.github.com>
2025-02-25 16:01:28 +0200

    Added that InputNode emits signal, OutputNode listens to it and triggers its own signal

--- scenes/gameScene/inputNode/input_node.gd
+++ scenes/gameScene/inputNode/input_node.gd
@@ -1,4 +1,7 @@
 extends Button
 
+signal input_activated
+
 func _on_pressed() -> void:
 	print(name, " pressed")
+	input_activated.emit()
--- scenes/gameScene/outputNode/output_node.gd
+++ scenes/gameScene/outputNode/output_node.gd
@@ -1,5 +1,13 @@
-extends Button
+extends ColorRect
 
+signal output_activated
 
-func _on_input_node_pressed() -> void:
+func _ready() -> void:
+	var input_node = get_parent().get_node("InputNode")
+	input_node.input_activated.connect(_on_input_node_activated)
+
+func _on_input_node_activated() -> void:
 	print(name, " got signal")
+	output_activated.emit()
+	
+	
