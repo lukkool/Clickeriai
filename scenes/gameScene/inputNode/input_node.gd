ccf2f5898035f1d0716252f8d07e90a8661d6311
Rokas Barodicas <bliteum1@gmail.com>
2025-02-28 16:37:18 +0200

    Added some variables to keep track of upgrades

--- scenes/gameScene/inputNode/input_node.gd
+++ scenes/gameScene/inputNode/input_node.gd
@@ -5,3 +5,14 @@ var score_generated = 1
 
 func _on_pressed() -> void:
 	input_activated.emit(score_generated)
+
+
+var dense_layer_nodes:int = 0
+var convolutional_layer_nodes:int = 0
+var recurrent_layer_nodes:int = 0
+var batch_normalization_layer_nodes:int = 0
+
+var upgr_auto_input_node1:bool = false
+var upgr_auto_input_node2:bool = false
+var upgr_auto_input_node3:bool = false
+var upgr_auto_input_node4:bool = false
