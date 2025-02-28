8249ea9ae3cff31f01b0f6b4ddee0c0cf147146c
Lukas Lisauskas <90777750+lukkool@users.noreply.github.com>
2025-02-28 14:45:42 +0200

    Added anchor points to input/output nodes. Changed label font scaling coefficient and made min font size based on height of the screen

--- scenes/gameScene/currency_label.gd
+++ scenes/gameScene/currency_label.gd
@@ -1,6 +1,9 @@
 extends Label
 
-var min_font_size: int = 40
+var min_font_size : int
+
+func _ready() -> void:
+	min_font_size = get_viewport_rect().size.y * 0.07
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -9,6 +12,6 @@ func _process(delta: float) -> void:
 
 func update_font_size() -> void:
 	var viewport_size = get_viewport_rect().size
-	var new_font_size = max(min_font_size, viewport_size.y * 0.05)
+	var new_font_size = max(min_font_size, viewport_size.y * 0.07)
 	add_theme_font_size_override("font_size", new_font_size)
 	
