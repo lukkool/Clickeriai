f3b290707666b5d5fc1038ebf8e52a0369559f82
Rokas <bliteum1@gmail.com>
2025-03-02 21:41:02 +0200

    Fixed upgrade save location

--- scenes/gameScene/gameScene.gd
+++ scenes/gameScene/gameScene.gd
@@ -11,3 +11,14 @@ var score:int:
 
 func _ready() -> void:
 	output_node.output_activated.connect(func(val): score += val)
+
+
+var dense_layer_nodes:int = 0
+var convolutional_layer_nodes:int = 0
+var recurrent_layer_nodes:int = 0
+var batch_normalization_layer_nodes:int = 0
+
+var upgr_auto_input_node1:bool = false
+var upgr_auto_input_node2:bool = false
+var upgr_auto_input_node3:bool = false
+var upgr_auto_input_node4:bool = false
--- scenes/gameScene/inputNode/input_node.gd
+++ scenes/gameScene/inputNode/input_node.gd
@@ -35,14 +35,3 @@ func _on_pressed() -> void:
 		self.modulate = multiplier_color
 	score_generated = multiplier * 1
 	input_activated.emit(score_generated)
-
-
-var dense_layer_nodes:int = 0
-var convolutional_layer_nodes:int = 0
-var recurrent_layer_nodes:int = 0
-var batch_normalization_layer_nodes:int = 0
-
-var upgr_auto_input_node1:bool = false
-var upgr_auto_input_node2:bool = false
-var upgr_auto_input_node3:bool = false
-var upgr_auto_input_node4:bool = false
