56d905087d81e2787c6acc098defcd3d6322e7d5
Lukas Lisauskas <90777750+lukkool@users.noreply.github.com>
2025-03-02 19:31:20 +0200

    Added click multiplier system to input node. After a certain amount of clicks multiplier activates for some time

--- scenes/gameScene/inputNode/input_node.gd
+++ scenes/gameScene/inputNode/input_node.gd
@@ -1,7 +1,39 @@
 extends Button
 
 signal input_activated(val)
+var multiplier = 1
 var score_generated = 1
 
+var click_streak = 0
+var needed_threshold = 20
+var multiplier_timer: float = 0.0
+var multiplier_duration: float = 10.0
+var multiplier_active = false
+var normal_color = Color(1, 1, 1)
+var multiplier_color = Color(1, 0, 0)
+
+
+func _process(delta: float) -> void:
+	if multiplier_active:
+		multiplier_timer -= delta
+		if multiplier_timer <= 0:
+			multiplier_active = false
+			multiplier = 1
+			self.modulate = normal_color
+	
+
+
 func _on_pressed() -> void:
+	if not multiplier_active:
+		click_streak += 1
+	print(click_streak)
+	if(click_streak > needed_threshold):
+		multiplier *= 2
+		click_streak = 0
+		multiplier_active = true
+		multiplier_timer = multiplier_duration
+		self.modulate = multiplier_color
+	score_generated = multiplier * 1
 	input_activated.emit(score_generated)
+	
+	
