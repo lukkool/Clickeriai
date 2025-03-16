0a7828d2d9b3d36ade3b6cc990bf076c7c58ef20
Lukas Lisauskas <90777750+lukkool@users.noreply.github.com>
2025-03-16 13:34:12 +0200

    Added progress bar for multiplier clicks

--- scenes/gameScene/inputNode/input_node.gd
+++ scenes/gameScene/inputNode/input_node.gd
@@ -12,6 +12,8 @@ var multiplier_active = false
 var normal_color = Color(1, 1, 1)
 var multiplier_color = Color(1, 0, 0)
 
+@onready var progress_bar: ProgressBar = get_node("/root/GameScene/ProgressBar")
+
 
 func _process(delta: float) -> void:
 	if multiplier_active:
@@ -24,10 +26,13 @@ func _process(delta: float) -> void:
 
 
 func _on_pressed() -> void:
-	if not multiplier_active: click_streak += 1
+	if not multiplier_active:
+		click_streak += 1
+		progress_bar.value += 1
 	if(click_streak > needed_threshold):
 		multiplier *= 2
 		click_streak = 0
+		progress_bar.value = 0
 		multiplier_active = true
 		multiplier_timer = multiplier_duration
 		self.modulate = multiplier_color
