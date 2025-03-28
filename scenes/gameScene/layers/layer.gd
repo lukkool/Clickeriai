d3b49653b3ca55bcc77728070158e451790f3eca
Rokas Barodicas <bliteum1@gmail.com>
2025-03-28 09:30:33 +0200

    Generalized the layer script

--- scenes/gameScene/layers/layer.gd
+++ scenes/gameScene/layers/layer.gd
@@ -2,10 +2,11 @@ extends VBoxContainer
 
 var enabled = false
 @onready var output_node = $%OutputNode
+@export var layer_number:int
 
 func enable():
 	if enabled: return
 	enabled = true
 	
 	visible = true
-	output_node.connect_last_layer_to(2)
+	output_node.connect_last_layer_to(layer_number)
