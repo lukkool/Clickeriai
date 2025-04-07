55457a5ad9fe9d444f9dda96dc4dcc183f71558d
Lukas Lisauskas <90777750+lukkool@users.noreply.github.com>
2025-04-07 19:56:28 +0300

    Output value now shows above output node and floats up

--- scenes/gameScene/outputNode/output_node.gd
+++ scenes/gameScene/outputNode/output_node.gd
@@ -1,15 +1,19 @@
 extends TextureRect
 
+
 signal output_activated(val)
 
 var nodes = [self]
 var enabled = true
 
 func _ready() -> void:
+	
 	for node in get_tree().get_nodes_in_group("nodes_layer0"):
 		node.score_output.connect(on_signal_get)
 
-func on_signal_get(val): output_activated.emit(val)
+func on_signal_get(val):
+	output_activated.emit(val)
+	show_number(val)
 
 
 var current_last_layer:int = 0
@@ -26,3 +30,27 @@ func reconnect_last_layer(current, next):
 		node.score_output.connect(on_signal_get)
 
 func update(): pass
+
+
+func show_number(value: int):
+	
+	var label = Label.new()
+	label.text = "+" + str(value)
+	var offset = Vector2(35 - (label.text.length() * 5.5), -30)
+	label.modulate = Color(1, 1, 1, 1)
+	label.position = offset
+	add_child(label)
+	
+	var tween = get_tree().create_tween()
+	tween.tween_property(
+		label, 'position',
+		offset - Vector2(0, 60), 0.9
+	).set_trans(Tween.TRANS_SINE)
+		
+	tween.tween_property(label, 'modulate:a', 0.0, 0.6)
+	tween.connect(
+		"finished",
+		func():
+			label.queue_free()
+	)
+		
